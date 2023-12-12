2c5c3c122c7d48541e3ff50cac5619cbabf9415f
Micael Wollter <micael.wollter@gmail.com>
2023-12-12 12:51:24 +0100

    Add Ball.cs

---
+++ Game/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public enum BallTypes
+    {
+        Win = 1,
+        ExraPick = 2,
+        NoWin = 3,
+    }
+
+    public class Ball
+    {
+        public BallTypes Type { get; set; }
+    }
+}
