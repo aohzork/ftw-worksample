19e59b187df29fd2846de1cb36c10e3778af55ff
Micael Wollter <micael.wollter@gmail.com>
2023-12-13 09:28:41 +0100

    Add initial ballGame no-wip

--- Game/BallGame.cs
+++ Game/BallGame.cs
@@ -9,16 +9,16 @@ namespace Game
 {
     public class BallGame
     {
-        private readonly int _costOfNewRound = 10;
-        private readonly int _creditsOnWin = 20;
+        private const int _costOfNewRound = 10;
+        private const int _creditsOnWin = 20;
 
         private List<IBall> _balls;
-        void GetBalls(List<IBall> balls)
+        public BallGame(List<IBall> balls)
         {
             _balls = balls;
         }
 
-        void NewSession(Player player)
+        public void NewSession(Player player)
         {
             var wonCredits = 0;
             var lostCredits = 0;
@@ -26,7 +26,7 @@ namespace Game
             string input = "";
             var canPlay = player.CanPlayRound(_costOfNewRound);
 
-            while(canPlay)
+            while (canPlay)
             {
                 var result = 0;
 
@@ -46,37 +46,33 @@ namespace Game
 
                 if (input.ToLower() == "n")
                 {
-                    canPlay = false;
                     EndSession(wonCredits, lostCredits);
+                    canPlay = false;
                 }
             }
         }
 
         string ResultChecker(int result, bool canExtraPick, Player player, int winAmount)
         {
-            var input = "";
+            
             switch (result)
             {
                 case 1:
                     Console.WriteLine("You Won the round!");
                     player.AddCredits(_creditsOnWin);
                     winAmount += _costOfNewRound;
-                    Console.Write("Would you like to play again? Y/N: ");
-                    input = Console.ReadLine();
                     break;
                 case 2:
-                    Console.WriteLine("Too bad. you Lost the round! But dont give up yet. Try again?");
-                    Console.Write("Y/N: ");
-                    input = Console.ReadLine();
+                    Console.WriteLine("Too bad. you Lost the round! But dont give up yet");
                     break;
                 case 3:
                     Console.WriteLine("Congrats!! You got got an Extra Pick!");
-                    input = "Y";
                     canExtraPick = true;
-                    break;
+                    return "Y";
             }
 
-            return input;
+            Console.Write("Would you like to play again? Y/N: ");
+            return Console.ReadLine();
         }
 
         void EndSession(int totalWonCredits, int totalLostCredits )
@@ -90,7 +86,7 @@ namespace Game
 
         //EndRound();
 
-        public int NewRound()
+        private int NewRound()
         {
             var rnd = new Random();
             var ball = PickBall(rnd);
